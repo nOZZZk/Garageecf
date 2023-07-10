e46396aaf8c184ad866dc79978519e45a31fc4fe
nOZZZk <c.gelant@hotmail.fr>
2023-07-10 09:17:20 +0200

    progession 1

---
+++ Véhicule.php
@@ -0,0 +1,13 @@
+<?php
+    require_once __DIR__ . "/Lib/menu.php"; 
+    require_once __DIR__ . "/templates/header.php";
+?>
+
+    <h1> NOS OCCASIONS </h1>
+
+
+
+
+<?php
+    require_once __DIR__ . "/templates/footer.php";  
+?>
